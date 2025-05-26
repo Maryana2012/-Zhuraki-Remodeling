0e62ba8331a0904a69cd6afa563df7e3406d090a
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2025-05-26 23:39:33 +0300

    testimonials form

--- js/review-modal.js
+++ js/review-modal.js
@@ -1,9 +1,6 @@
 const openModalBtn = document.querySelectorAll('.modal-open');
 const closeModalBtn = document.querySelector('.modal-close');
 const modal = document.querySelector('.backdrop');
-// console.log(openModalBtn)
-// console.log(closeModalBtn)
-// console.log(modal)
 
 openModalBtn.forEach(button=> button.addEventListener("click",
   (event=>{
