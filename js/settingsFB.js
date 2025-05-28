d1c1af6ad073cce96d04e391bb92dcc734f4ceea
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2025-05-28 20:06:49 +0300

    update settings for swiper testimonials

--- js/settingsFB.js
+++ js/settingsFB.js
@@ -42,30 +42,57 @@
 
   // рендер на сторінці відгуків
 
+let swiperTestimonial;
+
 function renderReviewsLive() {
   const reviewsList = document.getElementById("reviews-list");
-  const reviewsRef = collection(db, "reviews"); // заміни "reviews" на свою колекцію
+  const reviewsRef = collection(db, "reviews");
 
   onSnapshot(reviewsRef, (snapshot) => {
-    reviewsList.innerHTML = ""; // очищаємо список
+    reviewsList.innerHTML = "";
 
     snapshot.forEach((doc) => {
       const data = doc.data();
-// console.log(data)
       const reviewItem = document.createElement("div");
-      reviewItem.classList.add("swiper-slide");
-      reviewItem.classList.add('testimonials__item')
-    const date = data.created?.toDate().toLocaleDateString() || "Без дати";
- 
-    reviewItem.innerHTML = `
+      reviewItem.classList.add("swiper-slide", "testimonials__item");
+      const date = data.created?.toDate().toLocaleDateString() || "Без дати";
+
+      reviewItem.innerHTML = `
         <p class="text__article">${data.review || ""}</p>
         <div>
-        <p class="text__article">${data.name || "Анонім"},  ${data.city || ""} </p>    
-        <p class="text__article testimonial__text">${date || ""}</p>
+          <p class="text__article">${data.name || "Анонім"}, ${data.city || ""}</p>    
+          <p class="text__article testimonial__text">${date}</p>
         </div>
       `;
 
       reviewsList.appendChild(reviewItem);
+    });
+
+    // Оновлюємо або ініціалізуємо Swiper після того як DOM оновився
+    if (swiperTestimonial) swiperTestimonial.destroy(true, true);
+
+    swiperTestimonial = new Swiper('.testimonialSwiper', {
+      navigation: {
+        nextEl: '.testimonial__next',
+        prevEl: '.testimonial__prev',
+      },
+      loop: true,
+      spaceBetween: 20,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        375: {
+          slidesPerView: 1,
+        },
+        744: {
+          slidesPerView: 2,
+        },
+        1440: {
+          slidesPerView: 3,
+        }
+      }
     });
   });
 }
--- js/swiper.js
+++ js/swiper.js
@@ -20,28 +20,3 @@ const swiper = new Swiper('.mySwiper', {
     swiper: swiperThumbs,
   },
 });
-
-const swiperTestimonial = new Swiper('.testimonialSwiper', {
-  navigation: {
-    nextEl: '.testimonial__next',
-    prevEl: '.testimonial__prev',
-  },
-  loop: true,
-  spaceBetween: 20,
-  autoplay: {
-  delay: 2000, 
-    disableOnInteraction: false, 
-  },
-   breakpoints: {
-    375: {
-      slidesPerView: 1,
-    },
-    744: {
-      slidesPerView: 2,
-    },
-    1440: {
-      slidesPerView: 3,
-    }
-  }
-
-});
