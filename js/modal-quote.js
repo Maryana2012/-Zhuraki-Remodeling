dc36febd6db4cde9cad439cb3c8cfaa156dd16c5
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2025-06-03 17:42:46 +0300

    fixe header and modal quote

---
+++ js/modal-quote.js
@@ -0,0 +1,38 @@
+// const buttonOpen = document.querySelectorAll('.modal__quote-open');
+// const backdrop = document.querySelector('.backdrop__qoute');
+// const modal = document.querySelector('.modal__quote')
+// const closeModal = document.querySelector('.modal__quote-close')
+// console.log(modal)
+    
+
+// buttonOpen.forEach(el => {
+//     el.addEventListener(e => {
+//         backdrop.classList.remove('is-hidden');
+        
+//         })
+// })
+
+const buttonOpen = document.querySelectorAll('.modal__quote-open');
+const closeModal = document.querySelector('.modal__quote-close')
+const modal = document.querySelector('.backdrop__qoute');
+
+buttonOpen.forEach(button=> button.addEventListener("click",
+  (event=>{
+    const clickedButton = event.currentTarget;
+    
+    if(clickedButton){
+      toggleModal();
+    }
+    
+  })
+))
+
+if(closeModal){
+    closeModal.addEventListener("click", toggleModal);
+} 
+
+function toggleModal() {
+  modal.classList.toggle("is-hidden");
+}
+
+    
