6e8a56dc1bac4f39ff386ffba3a8f7a41a73f0bd
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2025-06-04 18:15:10 +0300

    mobile menu

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,11 +1,11 @@
 const buttonOpen = document.querySelector('.button__menu');
 const buttonClose = document.querySelector('.button__menu-close')
-const modal = document.querySelector('.backdrop__menu-mobile')
+const menu = document.querySelector('.menu-mobile-container')
 
 buttonOpen.addEventListener("click", toggleModal);
 buttonClose.addEventListener("click", toggleModal);
 
-
 function toggleModal() {
-    modal.classList.toggle("is-hidden");
+    menu.classList.toggle("is-hidden");
 }
+
